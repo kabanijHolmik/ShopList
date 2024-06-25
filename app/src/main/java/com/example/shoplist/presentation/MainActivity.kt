bf953e3f2fc85d1206cb6e33cfdfe8fa8c388886
kabanijHolmik <kosenkov809@gmail.com>
2024-06-25 12:21:07 +0300

    Add folders

--- app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.example.shoplist
+package com.example.shoplist.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.shoplist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
