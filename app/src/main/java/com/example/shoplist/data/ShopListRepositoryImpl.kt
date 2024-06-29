c368c053c441bf6a2a9653c9e8339a02ced485c0
kabanijHolmik <kosenkov809@gmail.com>
2024-06-29 23:58:18 +0300

    Create MainViewModel

--- app/src/main/java/com/example/shoplist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoplist/data/ShopListRepositoryImpl.kt
@@ -1,16 +1,26 @@
 package com.example.shoplist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoplist.domain.ShopItem
 import com.example.shoplist.domain.ShopListRepository
 import kotlin.jvm.Throws
 
 object ShopListRepositoryImpl: ShopListRepository {
+    private val shopListLiveData = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
 
     private var autoIncrementId = 0
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toMutableList()
+    init {
+        for (i in 0..9){
+            shopList.add(ShopItem("object $i", 1, i%2 == 1, i))
+        }
+    }
+
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        updateShopListLiveData()
+        return shopListLiveData
     }
 
     override fun getShopItem(shopItemId: Int): ShopItem? {
@@ -18,20 +28,26 @@ object ShopListRepositoryImpl: ShopListRepository {
     }
 
     override fun addShopItem(shopItem: ShopItem) {
-        if (shopItem.id == ShopItem.UNDEFINED_ID)
+        if (shopItem.id == ShopItem.UNDEFINED_ID){
         shopItem.id = autoIncrementId++
+        }
         shopList.add(shopItem)
+        updateShopListLiveData()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.removeAt(shopItem.id)
+        updateShopListLiveData()
     }
 
     override fun updateShopItem(shopItem: ShopItem) {
         val oldShopItem = getShopItem(shopItem.id)
         oldShopItem?.let { deleteShopItem(it) }
         addShopItem(shopItem)
+        updateShopListLiveData()
     }
 
-
+    private fun updateShopListLiveData(){
+        shopListLiveData.value = shopList.toList()
+    }
 }
--- app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.shoplist.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoplist.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainViewModel
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mainViewModel.shopList.observe(this){
+            Log.d("MainActivity", "$it")
+            val item = it[0]
+            if (count == 0){
+            mainViewModel.deleteShopItem(item)
+            count++
+            mainViewModel.changeEnableState(item)
+            }
+        }
         setContentView(R.layout.activity_main)
     }
 }
