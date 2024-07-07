1f72bf0f6cc512405fc6bb3db728c73e510facd9
kabanijHolmik <kosenkov809@gmail.com>
2024-07-07 18:12:50 +0300

    Create ShopListDiffCallback

--- app/src/main/java/com/example/shoplist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoplist/presentation/ShopListAdapter.kt
@@ -1,11 +1,11 @@
 package com.example.shoplist.presentation
 
 import android.util.Log
-import android.view.DragEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoplist.R
 import com.example.shoplist.domain.ShopItem
@@ -20,8 +20,10 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
 
     var shopList = listOf<ShopItem>()
         set(value) {
+            val callback = ShopListDiffCallback(shopList, value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
 
     class ShopItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
